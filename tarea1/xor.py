1aea32f6448963f219f79451bc8030800bc9dd72
dianagr95 <diana_gr95@hotmail.com>
2017-02-15 15:54:31 -0600

    algo

--- tarea1/xor.py
+++ tarea1/xor.py
@@ -68,3 +68,40 @@ class XOR:
     def poli_(self):
         return "WUT"
         
+        def get_grado(self, i):
+        g = 0
+        while i > 1:
+            i >>= 1
+            g += 1
+        return g
+    
+    def redu(self, a):
+        k = 0
+        g = self.get_grado(a)
+        while g >= 8:
+            aux = 0
+            if a & (1 << g):
+                aux = 1 << g - 8
+            aux = self.mult_aux(self.pol, aux)
+            a ^= aux
+            g = self.get_grado(a)
+        return a
+    
+    def mult_aux(self, bmult, b):
+        res = 0
+        i = 1
+        for x in range(self.get_grado(bmult) + 1):
+            if bmult & i:
+                res ^= b << x
+            i <<= 1
+        return res
+    
+    def mult(self):
+        bmult = int("AA", 16)
+        res = bytearray()
+        i  = 0
+        for b in self.bytesA:
+            print(str(i) + " Byte origen: " + bin(self.bytesA[i]) + " Byte a muliplicar: " + bin(bmult))
+            res.append(self.redu(self.mult_aux(bmult, b)))
+            i += 1
+        self.escribe_archivo("multiplicacion.out", res)
