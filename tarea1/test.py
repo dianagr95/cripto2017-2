f94410910285ec433e503952bd2a10a0841333a2
GrandPaladin <grand_paladin@ciencias.unam.mx>
2017-02-15 22:03:11 -0600

    Agrega las pruebas y modifica el archivo tex.py

--- tarea1/test.py
+++ tarea1/test.py
@@ -7,8 +7,17 @@ if __name__ == "__main__":
 
     fileM = FileManager()
 
+    out = fileM.loadFile("xor.out")
+    out1 = fileM.loadFile("xor1.out")
+    print("xor")
+    if out == out1:
+        print("yei")
+    else:
+        print("nain")
+
     out = fileM.loadFile("multiplicacion.out")
     out1 = fileM.loadFile("multiplicacion1.out")
+    print("mul")
     if out == out1:
         print("yei")
     else:
